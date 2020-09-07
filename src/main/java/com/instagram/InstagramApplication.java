23b0b82302b861e0a8b553b7d9e1d85e74234658
Torres Yang <yangguanhua@gmail.com>
2020-09-07 12:23:18 -0700

    Added in mongodb jdbc url

--- src/main/java/com/instagram/InstagramApplication.java
+++ src/main/java/com/instagram/InstagramApplication.java
@@ -1,4 +1,4 @@
-package com.torres.instagram;
+package com.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/instagram/InstagramApplicationTests.java
+++ src/test/java/com/instagram/InstagramApplicationTests.java
@@ -1,4 +1,4 @@
-package com.torres.instagram;
+package com.instagram;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
